a468b54b9193e1f6d5422cfa8968d4caa599b6be
MDhanesh <mgdhanesh98@gmail.com>
2022-10-21 19:02:21 +0530

    n

--- router/employeeRouter.js
+++ router/employeeRouter.js
@@ -11,3 +11,9 @@ router.put("/update/:id", employeeModule.updateEmployees);
 router.delete("/delete/:id", employeeModule.deleteEmployees);
 
 module.exports = router;
+
+
+MONGODB_URL=mongodb+srv://XXX:FksMCyYcYMgkUkK9@project.66wonva.mongodb.net/?retryWrites=true&w=majority
+PORT=3001
+
+SECERT_KEY=ABCD
